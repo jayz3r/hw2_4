d89f7d875ebdfe618782c08fd56a8faf1bcc51ab
Adil <adil.abdyldaev07@gmail.com>
2024-07-06 21:19:58 +0600

    hw1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function Header() {
+  return(
+    <>
+    <Title/>
+    </>
+  )
+}
+
+
+function Content() {
+  return(
+    <>
+    <Title/>
+    </>
+  )
+}
+
+function Footer() {
+  return(
+    <>
+    <Title/>
+      </>
+  )
+}
+
+function Title() {
+  return(
+    <>
+    <h1>I am title component</h1>
+    </>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Header/>
+   <Content/>
+   <Footer/>
+   </>
   );
 }
 
